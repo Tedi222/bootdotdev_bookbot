50c427492493d07e979096ccb60d0a25aab00df7
Tedi222 <teodor.pstrusinski@gmail.com>
2025-06-11 22:45:21 +0200

    add function that takes string as input and returns a dict of each character count

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words
+from stats import get_num_words, get_character_count
+
 def get_book_text(book_path):
     with open(book_path) as f:
         book_contents = f.read()
@@ -8,4 +9,6 @@ def get_book_text(book_path):
 if __name__ == "__main__":
     book = get_book_text("./books/frankenstein.txt")
     num_words = get_num_words(book)
+    characters = get_character_count(book)
     print(f"{num_words} words found in the document")
+    print(characters)
--- stats.py
+++ stats.py
@@ -1,3 +1,11 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_character_count(text):
+    text = text.lower()
+    characters_set = set(text)
+    characters_dict = {}
+    for c in characters_set:
+        characters_dict[c] = text.count(c)
+    return characters_dict
